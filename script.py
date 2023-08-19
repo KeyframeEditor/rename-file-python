f33618b574fc434eff206e99cb7ed46c69414f0e
Andhika_R <101549113+KeyframeEditor@users.noreply.github.com>
2023-08-19 16:18:17 +0700

    add python script file

---
+++ script.py
@@ -0,0 +1,40 @@
+import os, re
+
+# Set the directory path where your files are stored
+directory = r'YOUR FOLDER PATH'
+
+# Set the file data type (eg. .png, .jpg, .jpeg)
+dataType = ".png"
+
+
+# Get the list of file names in the directory
+file_list = os.listdir(directory)
+
+# Sort the file names in ascending order using natural sorting
+def natural_sort_key(s):
+    return [int(s) if s.isdigit() else s.lower() for s in re.split(r'(\d+)', s)]
+file_list.sort(key=natural_sort_key)
+
+# Initialize the counter to 0
+counter = 0
+
+# Loop through all files in the directory
+for filename in file_list:
+    print(filename)
+    # Check if the file is a image file
+    if filename.endswith(dataType):
+        # Construct the new filename with the counter and file extension
+        new_filename = str(counter) + dataType
+        # Construct the full path for the old and new file names
+        old_filepath = os.path.join(directory, filename)
+        new_filepath = os.path.join(directory, new_filename)
+        # Check if the new file name already exists
+        while os.path.exists(new_filepath):
+            # If it does, append a number to the end of the file name
+            counter += 1
+            new_filename = str(counter) + dataType
+            new_filepath = os.path.join(directory, new_filename)
+        # Rename the file
+        os.rename(old_filepath, new_filepath)
+        # Increment the counter
+        counter += 1
